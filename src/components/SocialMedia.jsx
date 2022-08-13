c42f44c0f14f89b3c39c1fc25cf2186cb9973252
muhammad_alif_d <muhammad_alif_d@students.itb.ac.id>
2022-08-13 20:14:06 +0700

    moving such image directory to src

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import email from '../assets/email.png';
+import instagram from '../assets/instagram.png';
+import linkedin from '../assets/linkedin.png'
+import github from '../assets/github.png'
 
 export function SocialMedia() {
   return (
     <div className='social-media'>
       <div className='account-btn' onClick={() => window.location = 'https://github.com/alif338'}>
-        <img src='/assets/github.png' alt='github'/>
+        <img src={github} alt='github'/>
       </div>
       <div className='account-btn' onClick={() => window.location = 'https://instagram.com/aliefdharm'}>
-        <img src='/assets/instagram.png' alt='instagram'/>
+        <img src={instagram} alt='instagram'/>
       </div>
       <div className='account-btn' onClick={() => window.location = 'https://www.linkedin.com/in/muhammad-alif-d-4aaa561a9/'}>
-        <img src='/assets/linkedin.png' alt='linkedin'/>
+        <img src={linkedin} alt='linkedin'/>
       </div>
       <div className='account-btn' onClick={() => window.location = 'mailto:m.alifdarmamulia@gmail.com'}>
-        <img src='/assets/email.png' alt='email'/>
+        <img src={email} alt='email'/>
       </div>
     </div>
   )
